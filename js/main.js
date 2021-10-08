0183b4e1187ae4c605e5644a21481743e3759a6c
carlos <caaz160782@gmail.com>
2021-10-08 04:07:53 -0500

    se agrego funcionalidad al select de tags

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-// Add Your Scripts here
+//https://devpost-72887-default-rtdb.firebaseio.com/tags
+//obtiene tags
+const getTags = () => {
+    let tags
+    $.ajax({
+        method: "GET",
+        url: "https://devpost-72887-default-rtdb.firebaseio.com/tags.json",
+        success: response => { 
+            // console.log('response al terminar la peticion',response)
+            tags = response
+           // llenaTags(tags)
+        },
+        error: error => {
+            console.log(error)
+        },
+        async: false
+    })    
+    return tags
+}
+ //pintar slect //id="tags"
+ const llenaTags =(tags )=>{
+    for (const key in tags) {
+       $("#tags").append('<option value=' + key + '>' + tags[key] + '</option>');        
+    }   
+ }
+ llenaTags(getTags())
+  //llena input
+let arrayTags=[]
+const llenaInpuTag=()=>{
+   //let select_item = $("#tags").val()
+    let select_text = $("#tags option:selected").text()
+    $("#tags option:selected").remove()
+     arrayTags=[...arrayTags, select_text]     
+    $("#inputTags").val(arrayTags.toString())
+ }
+//metodo change en tag
+$("#tags").change(function() {
+   //llenamos el input 
+   llenaInpuTag()
+})
